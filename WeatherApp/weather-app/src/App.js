7be822411b932b9fb695c0f1df60145e5d14efc8
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-11-08 16:01:57 +0100

    Weather App added

---
+++ WeatherApp/weather-app/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import LocationListContainer from './containers/LocationListContainer';
+import ForecastExtendedContainer from './containers/ForecastExtendedContainer';
+import { Grid, Col, Row } from 'react-flexbox-grid';
+import './App.css';
+
+const cities = [
+  'Almeria,es',
+  'Malaga,es',
+  'Cadiz,es',
+  'Granada,es',
+  'Huelva,es',
+  'Sevilla,es',
+  'Jaen,es',
+  'Cordoba,es',
+]
+
+class App extends Component {
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationListContainer 
+              cities={cities} 
+            >
+           </LocationListContainer>
+          </Col>
+          <Col xs={12} md={6}>
+            <Paper elevation={4}>
+              <div className="details">
+                  <ForecastExtendedContainer></ForecastExtendedContainer>     
+              </div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+export default App;
+
